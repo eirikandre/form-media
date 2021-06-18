dfdf2bfe7b4f47f10b4b27b580fdc21a772dae2b
Eirik Eidså <eirik.eidsa@husbanken.no>
2021-06-18 21:28:59 +0200

    chmod

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-sudo cp ./radio.service /etc/systemd/system/radio.service
+HERE=$(pwd)
+
+sudo ln -s "$HERE/radio.service" "/etc/systemd/system/radio.service"
 
 sudo systemctl enable radio.service
 sudo systemctl start radio.service
